d686389986f91212a6ca637d135fae5ca823b5fb
Suhebji <Suhebkhansrk@gmail.com>
2025-07-03 15:20:17 +0530

    Update Pair.js

--- scripts/cmds/Pair.js
+++ scripts/cmds/Pair.js
@@ -1,55 +1,104 @@
-const axios = require ("axios");
-const fs = require ("fs-extra");
+const { GoatWrapper } = require('fca-liane-utils');
+const { loadImage, createCanvas } = require("canvas");
+const axios = require("axios");
+const fs = require("fs-extra");
 
 module.exports = {
   config: {
     name: "pair",
-    aliases: [],
-    version: "1.0",
-    author: "Karan jalvanshi",
-    countDown: 5,
+    author: "xemon",
     role: 0,
-    shortDescription: " ",
-    longDescription: "",
+    shortDescription: "Pair two people",
+    longDescription: "Randomly pairs two users based on gender",
     category: "love",
     guide: "{pn}"
   },
 
-  onStart: async function({ api, event, threadsData, usersData }) {
+  onStart: async function ({ api, event, usersData }) {
+    let pathImg = __dirname + "/cache/background.png";
+    let pathAvt1 = __dirname + "/cache/Avtmot.png";
+    let pathAvt2 = __dirname + "/cache/Avthai.png";
 
-    const { threadID, messageID, senderID } = event;
-    const { participantIDs } = await api.getThreadInfo(threadID);
-    var tle = Math.floor(Math.random() * 101);
-    var namee = (await usersData.get(senderID)).name
+    var id1 = event.senderID;
+    var name1 = (await usersData.get(id1))?.name || "User 1";
+
+    let ThreadInfo = await api.getThreadInfo(event.threadID);
+    let all = ThreadInfo.userInfo;
+    let gender1 = "OTHER";
+    
+    for (let user of all) {
+      if (user.id == id1) {
+        gender1 = user.gender === 1 ? "FEMALE" : user.gender === 2 ? "MALE" : "OTHER";
+      }
+    }
+
     const botID = api.getCurrentUserID();
-    const listUserID = participantIDs.filter(ID => ID != botID && ID != senderID);
-    var id = listUserID[Math.floor(Math.random() * listUserID.length)];
-    var name = (await usersData.get(id)).name
-    var arraytag = [];
-    arraytag.push({ id: senderID, tag: namee });
-    arraytag.push({ id: id, tag: name });
+    let candidates = [];
 
-    let Avatar = (await axios.get(`https://graph.facebook.com/${senderID}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`, { responseType: "arraybuffer" })).data;
-    fs.writeFileSync(__dirname + "/cache/avt.png", Buffer.from(Avatar, "utf-8"));
+    if (gender1 === "FEMALE") {
+      candidates = all.filter(u => u.gender === 2 && u.id !== id1 && u.id !== botID).map(u => u.id);
+    } else if (gender1 === "MALE") {
+      candidates = all.filter(u => u.gender === 1 && u.id !== id1 && u.id !== botID).map(u => u.id);
+    } else {
+      candidates = all.filter(u => u.id !== id1 && u.id !== botID).map(u => u.id);
+    }
 
-    let gifLove = (await axios.get(`https://i.ibb.co/y4dWfQq/image.gif`, { responseType: "arraybuffer" })).data;
-    fs.writeFileSync(__dirname + "/cache/giflove.png", Buffer.from(gifLove, "utf-8"));
+    if (candidates.length === 0) {
+      return api.sendMessage("⚠️ No suitable match found!", event.threadID);
+    }
 
-    let Avatar2 = (await axios.get(`https://graph.facebook.com/${id}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`, { responseType: "arraybuffer" })).data;
-    fs.writeFileSync(__dirname + "/cache/avt2.png", Buffer.from(Avatar2, "utf-8"));
+    let id2 = candidates[Math.floor(Math.random() * candidates.length)];
+    let name2 = (await usersData.get(id2))?.name || "User 2";
 
-    var imglove = [];
+    let matchPercent = Math.floor(Math.random() * 100) + 1;
+    let backgroundImages = [
+      "https://i.postimg.cc/wjJ29HRB/background1.png",
+      "https://i.postimg.cc/zf4Pnshv/background2.png",
+      "https://i.postimg.cc/5tXRQ46D/background3.png"
+    ];
+    let backgroundURL = backgroundImages[Math.floor(Math.random() * backgroundImages.length)];
 
-    imglove.push(fs.createReadStream(__dirname + "/cache/avt.png"));
-    imglove.push(fs.createReadStream(__dirname + "/cache/giflove.png"));
-    imglove.push(fs.createReadStream(__dirname + "/cache/avt2.png"));
+    let userInfo1 = await api.getUserInfo(id1);
+    let userInfo2 = await api.getUserInfo(id2);
+    let avatarURL1 = userInfo1[id1]?.thumbSrc || "";
+    let avatarURL2 = userInfo2[id2]?.thumbSrc || "";
 
-    var msg = {
-      body: `🥰Successful pairing!\n💌Wish you two hundred years of happiness\n💕Double ratio: ${tle}%\n${namee} 💓 ${name}`,
-      mentions: arraytag,
-      attachment: imglove
-    };
+    let getAvtmot = (await axios.get(avatarURL1, { responseType: "arraybuffer" })).data;
+    fs.writeFileSync(pathAvt1, Buffer.from(getAvtmot, "utf-8"));
 
-    return api.sendMessage(msg, event.threadID, event.messageID);
-  }
+    let getAvthai = (await axios.get(avatarURL2, { responseType: "arraybuffer" })).data;
+    fs.writeFileSync(pathAvt2, Buffer.from(getAvthai, "utf-8"));
+
+    let getBackground = (await axios.get(backgroundURL, { responseType: "arraybuffer" })).data;
+    fs.writeFileSync(pathImg, Buffer.from(getBackground, "utf-8"));
+
+    let baseImage = await loadImage(pathImg);
+    let baseAvt1 = await loadImage(pathAvt1);
+    let baseAvt2 = await loadImage(pathAvt2);
+    let canvas = createCanvas(baseImage.width, baseImage.height);
+    let ctx = canvas.getContext("2d");
+
+    ctx.drawImage(baseImage, 0, 0, canvas.width, canvas.height);
+    ctx.drawImage(baseAvt1, 100, 150, 300, 300);
+    ctx.drawImage(baseAvt2, 900, 150, 300, 300);
+
+    const imageBuffer = canvas.toBuffer();
+    fs.writeFileSync(pathImg, imageBuffer);
+    fs.removeSync(pathAvt1);
+    fs.removeSync(pathAvt2);
+
+    return api.sendMessage(
+      {
+        body: `💞Successful pairing!\n💌Wish you two hundred years of happiness. 🥰\nMay you both always be happy🙂\n👫 ${name1} + ${name2}\n❤️ Match Percentage: ${matchPercent}%`,
+        mentions: [{ tag: name2, id: id2 }],
+        attachment: fs.createReadStream(pathImg),
+      },
+      event.threadID,
+      () => fs.unlinkSync(pathImg),
+      event.messageID
+    );
+  },
 };
+
+const wrapper = new GoatWrapper(module.exports);
+wrapper.applyNoPrefix({ allowPrefix: true });
